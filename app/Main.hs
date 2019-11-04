ec73e3ca70681ee6c75b9abe3a61125eaa3bbbe4
Jilles Valk <jvalk@sogyo.nl>
2019-11-04 14:34:28 +0100

    Simple server for getting index.html and js.

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,37 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp (run)
 
-import Lib
-import Shapes
+app :: Application
+app request respond = respond $ case rawPathInfo request of
+    "/"     -> index
+    "/index.js" -> indexJS
+    _       -> notFound
+
+indexJS :: Response
+indexJS = responseFile
+    status200
+    [("Content-Type", "text/html")]
+    "./client/index.js"
+    Nothing
+
+notFound :: Response
+notFound = responseLBS
+    status404
+    [("Content-Type", "text/plain")]
+    "404 - Not Found"
+
+index :: Response
+index = responseFile
+    status200
+    [("Content-Type", "text/html")]
+    "./client/index.html"
+    Nothing
+
+
 
 main :: IO ()
-main = do 
-    someFunc
-    print (moveP (Point 1 2 3) (Vector 3 2 1))
+main = do
+    putStrLn $ "http://localhost:8000/"
+    run 8000 app
