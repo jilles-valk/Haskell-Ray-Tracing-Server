dd56ad01b1ac41d8339b366f2c7b730f22410247
Jilles Valk <jvalk@sogyo.nl>
2019-11-04 14:58:17 +0100

    Put server code in server module and added stylesheet to send.

--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
-import Network.Wai
-import Network.HTTP.Types
+import Server
 import Network.Wai.Handler.Warp (run)
-
-app :: Application
-app request respond = respond $ case rawPathInfo request of
-    "/"     -> index
-    "/index.js" -> indexJS
-    _       -> notFound
-
-indexJS :: Response
-indexJS = responseFile
-    status200
-    [("Content-Type", "text/html")]
-    "./client/index.js"
-    Nothing
-
-notFound :: Response
-notFound = responseLBS
-    status404
-    [("Content-Type", "text/plain")]
-    "404 - Not Found"
-
-index :: Response
-index = responseFile
-    status200
-    [("Content-Type", "text/html")]
-    "./client/index.html"
-    Nothing
-
 
 
 main :: IO ()
--- src/Server.hs
+++ src/Server.hs
@@ -1,9 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Network.Wai
-import Network.HTTP.Types (status200)
-import Network.Wai.Handler.Warp (run)
+module Server where
 
-application _ respond = respond $
-  responseLBS status200 [("Content-Type", "text/plain")] "Hello World"
+	import Network.Wai
+	import Network.HTTP.Types
+	import Network.Wai.Handler.Warp (run)
 
-main = run 3000 application
+	app :: Application
+	app request respond = respond $ case rawPathInfo request of
+		"/"     -> index
+		"/index.js" -> indexJS
+		"/style.css" -> styleCSS
+		_       -> notFound
+
+	indexJS :: Response
+	indexJS = responseFile
+		status200
+		[("Content-Type", "text/html")]
+		"./client/index.js"
+		Nothing
+
+	styleCSS :: Response
+	styleCSS = responseFile
+		status200
+		[("Content-Type", "text/html")]
+		"./client/style.css"
+		Nothing
+
+	notFound :: Response
+	notFound = responseLBS
+		status404
+		[("Content-Type", "text/plain")]
+		"404 - Not Found"
+
+	index :: Response
+	index = responseFile
+		status200
+		[("Content-Type", "text/html")]
+		"./client/index.html"
+		Nothing
