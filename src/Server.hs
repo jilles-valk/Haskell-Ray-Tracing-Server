32a085be2d9901233fbcf853bfdd5f863fff274b
Jilles Valk <jvalk@sogyo.nl>
2019-11-04 15:35:50 +0100

    A simple setup for index.html and index.css.

--- src/Server.hs
+++ src/Server.hs
@@ -1,40 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Server where
 
-	import Network.Wai
-	import Network.HTTP.Types
-	import Network.Wai.Handler.Warp (run)
+    import Network.Wai
+    import Network.HTTP.Types
+    import Network.Wai.Handler.Warp (run)
 
-	app :: Application
-	app request respond = respond $ case rawPathInfo request of
-		"/"     -> index
-		"/index.js" -> indexJS
-		"/style.css" -> styleCSS
-		_       -> notFound
+    app :: Application
+    app request respond = respond $ case rawPathInfo request of
+        "/"     -> index
+        "/index.js" -> indexJS
+        "/index.css" -> indexCSS
+        _       -> notFound
 
-	indexJS :: Response
-	indexJS = responseFile
-		status200
-		[("Content-Type", "text/html")]
-		"./client/index.js"
-		Nothing
+    indexJS :: Response
+    indexJS = responseFile
+        status200
+        [("Content-Type", "text/html")]
+        "./client/index.js"
+        Nothing
 
-	styleCSS :: Response
-	styleCSS = responseFile
-		status200
-		[("Content-Type", "text/html")]
-		"./client/style.css"
-		Nothing
+    indexCSS :: Response
+    indexCSS = responseFile
+        status200
+        [("Content-Type", "text/css")]
+        "./client/index.css"
+        Nothing
 
-	notFound :: Response
-	notFound = responseLBS
-		status404
-		[("Content-Type", "text/plain")]
-		"404 - Not Found"
+    notFound :: Response
+    notFound = responseLBS
+        status404
+        [("Content-Type", "text/plain")]
+        "404 - Not Found"
 
-	index :: Response
-	index = responseFile
-		status200
-		[("Content-Type", "text/html")]
-		"./client/index.html"
-		Nothing
+    index :: Response
+    index = responseFile
+        status200
+        [("Content-Type", "text/html")]
+        "./client/index.html"
+        Nothing
